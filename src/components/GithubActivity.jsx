6aa41cc122e27f4381d56990c230f8c121e21399
Clehue <clehue@ast.cl>
2026-08-24 17:31:12 -0400

    Refactor code structure for improved readability and maintainability

--- src/components/GithubActivity.jsx
+++ src/components/GithubActivity.jsx
@@ -5,10 +5,10 @@ import Title from "./Title.astro";
 export default function GithubGrid({ username }) {
   return (
     <section id="activity" className="max-w-7xl z-50 mx-auto flex flex-col md:py-10">
-      <div class="flex flex-col justify-center items-start text-center ms-5 md:ms-0">
+      <div class="animate-slide-in-left flex flex-col justify-center items-start text-center ms-5 md:ms-0">
         <h1 class="text-lg mb-4 md:mb-6 tracking-[0.12rem]">[ GitHub Activity ]</h1>
       </div>
-      <div className="relative  w-full grayscale-100  md:w-full mt-6 md:mt-10 flex flex-col md:flex-row justify-center items-center">
+      <div className="animate-fade-in-down relative  w-full grayscale-100  md:w-full mt-6 md:mt-10 flex flex-col md:flex-row justify-center items-center">
         <GitHubCalendar
           username={username}
           colorScheme="dark"
